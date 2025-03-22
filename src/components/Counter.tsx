4bd4fbf321843e4f0b2fc1c68c405c7930605fc4
Emilio Ziniades <emilioziniades@protonmail.com>
2025-03-22 13:25:02 +0200

    strip out all css

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,9 @@
 import { createSignal } from "solid-js";
-import "./Counter.css";
 
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button
-      class="increment"
-      onClick={() => setCount(count() + 1)}
-      type="button"
-    >
+    <button onClick={() => setCount(count() + 1)} type="button">
       Clicks: {count()}
     </button>
   );
