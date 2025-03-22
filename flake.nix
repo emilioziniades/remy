9d472c0c84f2227bb4f40cbf08423f453cdeb8c6
Emilio Ziniades <emilioziniades@protonmail.com>
2025-03-22 12:55:21 +0200

    update flake.nix for node development environment

--- flake.nix
+++ flake.nix
@@ -9,23 +9,13 @@
     self,
     nixpkgs,
   }: let
-    forAllSystems = fn:
-      nixpkgs.lib.genAttrs
-      ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"]
-      (system: fn system nixpkgs.legacyPackages.${system});
+    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = fn: nixpkgs.lib.genAttrs systems (system: fn system nixpkgs.legacyPackages.${system});
   in {
     devShells = forAllSystems (system: pkgs: {
       default = pkgs.mkShell {
         buildInputs = with pkgs; [
-          rustc
-          cargo
-          cargo-leptos
-          cargo-generate
-
-          dart-sass
-
-          # wasm tools seem to require LLD. See here: https://nixos.wiki/wiki/Rust#Using_LLD_instead_of_LD
-          llvmPackages.bintools
+          nodejs_22
         ];
       };
     });
